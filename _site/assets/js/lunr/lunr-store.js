c247675ac8e904e6424497235e1ccee8ccbbcde2
YangDongJae <ydj9805@me.com>
2020-06-16 20:06:05 +0900

    make blog with markdown, edit Cofig file , Navigation file for make categories

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "[yangdongjae]",
+        "excerpt":"Who Are 양동재? student of AI Software Engineering 소개 Github : https://github.com/YangDongJae Linkedin Mail : ydj9805@gmail.com 경력   기간 담당업무 Asia Economic Community Forum 2017.06.03 ~ 2017.12.31 2017 아시아 경제공동체 포럼 기획 Model Asia Union 국제 학술대회 기획 및 운영 협력업체 선정 및 국제 포럼 홍보 및 마케팅 수행 PRISM...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/YangDongJae/",
+        "teaser": null
+      }]
