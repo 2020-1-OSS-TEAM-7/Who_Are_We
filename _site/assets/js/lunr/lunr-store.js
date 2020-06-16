d2397655cc3043ac4c54fc38025a956add98f504
YangDongJae <ydj9805@me.com>
2020-06-16 20:30:15 +0900

    edit for categories connect

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,31 @@
 var store = [{
+        "title": "Wha Are Dansaka Junki?",
+        "excerpt":"Who Are Dansaka Junki?   ","categories": ["Dansaka","Junki"],
+        "tags": ["who_are_we?"],
+        "url": "http://localhost:4000/dansaka/junki/Dansaka-Junki/",
+        "teaser": null
+      },{
         "title": "Wha Are 양동재?",
         "excerpt":"Who Are 양동재? student of AI Software Engineering 소개 Github : https://github.com/YangDongJae Linkedin Mail : ydj9805@gmail.com 경력   기간 담당업무 Asia Economic Community Forum 2017.06.03 ~ 2017.12.31 2017 아시아 경제공동체 포럼 기획 Model Asia Union 국제 학술대회 기획 및 운영 협력업체 선정 및 국제 포럼 홍보 및 마케팅 수행 PRISM...","categories": ["양동재"],
         "tags": ["who_are_we?"],
         "url": "http://localhost:4000/%EC%96%91%EB%8F%99%EC%9E%AC/YangDongJae/",
+        "teaser": null
+      },{
+        "title": "Wha Are 구경현?",
+        "excerpt":"Who Are 구경현   ","categories": ["구경현"],
+        "tags": ["who_are_we?"],
+        "url": "http://localhost:4000/%EA%B5%AC%EA%B2%BD%ED%98%84/%EA%B5%AC%EA%B2%BD%ED%98%84/",
+        "teaser": null
+      },{
+        "title": "Wha Are 김재현?",
+        "excerpt":"Who Are 김재현   ","categories": ["김재현"],
+        "tags": ["who_are_we?"],
+        "url": "http://localhost:4000/%EA%B9%80%EC%9E%AC%ED%98%84/%EA%B9%80%EC%9E%AC%ED%98%84/",
+        "teaser": null
+      },{
+        "title": null,
+        "excerpt":"Who Are 오성빈   ","categories": [],
+        "tags": null,
+        "url": "http://localhost:4000/2020-06-16-%EC%98%A4%EC%84%B1%EB%B9%88/",
         "teaser": null
       }]
